f833674e702398663dcd189480de3a90753f4f85
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-08-24 20:23:18 -0400

    quase finalizando

--- estruturas.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-cobra = []
-
-tamanho_da_tela = (600, 400)
-
-def comeu (comida):
-    comida[0] = randint(1, 600)
-    comida[1] = randint(1, 400)
--- snake.py
+++ snake.py
@@ -1,69 +1,140 @@
 import pygame #biblioteca para desnvolver o jogo
 from pygame.locals import *
 from sys import exit
-import estruturas
 from random import randint
 
 pygame.init() #inicializa o pygame
 
-#variaveis cobra
-cabeça_cobra = [estruturas.tamanho_da_tela[0]/2, estruturas.tamanho_da_tela[1]/2]
+#cores
+verde = (0, 255, 0)
+vermelho = (255, 0, 0)
+preto = (0, 0, 0)
+branco = (255, 255, 255)
 
-corpo_cobra_x, corpo_cobra_y = cabeça_cobra[0], cabeça_cobra[1]
+#variaveis do jogo
+lista_cobra = []
+pontos = 0
+comprimento_inicial = 5
+velocidade = 10
+morreu = False
 
-comida = [randint(1, 600), randint(1, 400)]
+#coordenadas
+tamanho_tela = (800,600)
+cobra_x, cobra_y = 800/2, 600/2
+maca_x, maca_y = randint(1,800), randint(1,600)
+x_controle, y_controle = velocidade, 0
 
-tamanho_cobra = 0
-corpo_cobra = [cabeça_cobra]
-desenho_cobra = []
 
-#eventos do jogo
 fps = pygame.time.Clock()
-tela = pygame.display.set_mode(estruturas.tamanho_da_tela)
+tela = pygame.display.set_mode(tamanho_tela)
+pygame.display.set_caption("snake game")
+fonte = pygame.font.SysFont("arial", 40, True, True)
 
+#desenha a cobra
+def aumenta_cobra(lista_cobra):
+	for XeY in lista_cobra:
+		pygame.draw.rect(tela, verde, (XeY[0], XeY[1], 20, 20))
+
+def reinicar_jogo():
+	global pontos, comprimento_inicial, cobra_x, cobra_y, lista_cobra, lista_cabeca, maca_x, maca_y, morreu
+	pontos = 0
+	comprimento_inicial = 5
+	cobra_x, cobra_y = 800/2, 600/2
+	lista_cobra = []
+	lista_cabeca = []
+	maca_x, maca_y = randint(1,800), randint(1,600)
+	morreu = False
 
 while True:
 
-	tela.fill((0, 0, 0))
-	fps.tick(120)
+	tela.fill((branco))#pintando o fundo
+	fps.tick(30)#fps
+
+	#mensagem
+	mensagem = f'Pontos: {pontos}'
+	texto_formatado = fonte.render(mensagem, False, preto)
+
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
-			pygame.quit
+			pygame.quit()
 			exit()
 
-	#andando com a cobra
-	if pygame.key.get_pressed()[K_a]:
-		cabeça_cobra[0] = cabeça_cobra[0] - 2
-	if pygame.key.get_pressed()[K_d]:
-		cabeça_cobra[0] = cabeça_cobra[0] + 2
-	if pygame.key.get_pressed()[K_w]:
-		cabeça_cobra[1] = cabeça_cobra[1] - 2
-	if pygame.key.get_pressed()[K_s]:
-		cabeça_cobra[1] = cabeça_cobra[1] + 2
+		if event.type == KEYDOWN:
+			if event.key == K_a:
+				if x_controle == velocidade:
+					pass
+				else:
+					x_controle = -velocidade
+					y_controle = 0
 
+			if event.key == K_d:
+				if x_controle == -velocidade:
+					pass
+				else:
+					x_controle = velocidade
+					y_controle = 0
+
+			if event.key == K_w:
+				if y_controle == velocidade:
+					pass
+				else:
+					x_controle = 0
+					y_controle = -velocidade
+
+			if event.key == K_s:
+				if y_controle == -velocidade:
+					pass
+				else:
+					x_controle = 0
+					y_controle = velocidade
+
+	if cobra_x < 0:
+		cobra_x = tamanho_tela[0]
+	elif cobra_x > tamanho_tela[0]:
+		cobra_x = 0
+	if cobra_y < 0:
+		cobra_y = tamanho_tela[1]
+	elif cobra_y > tamanho_tela[1]:
+		cobra_y = 0
+
+	cobra_x += x_controle
+	cobra_y += y_controle
 	#desenhando
-	comida = pygame.draw.rect(tela, (255, 0, 0), (comida[0], comida[1], 10, 10))
+	cobra = pygame.draw.rect(tela, verde, (cobra_x, cobra_y, 20,20))
+	maca = pygame.draw.rect(tela, vermelho,(maca_x, maca_y, 20, 20)) 
 
-	cabeça_cobra = pygame.draw.rect(tela, (0, 255, 0),(cabeça_cobra[0], cabeça_cobra[1], 20, 20 ))
+	#verifica colisão da cobra
+	if cobra.colliderect(maca):
+		maca_x = randint(40, 800)
+		maca_y = randint(50, 600)
+		pontos = pontos + 1
+		comprimento_inicial += 1
+
+	#armazena as posiçoes da cabeça da cobra
+	lista_cabeca = []
+	lista_cabeca.append(cobra_x)
+	lista_cabeca.append(cobra_y)
+
+	#armazena todos os valores da cobra
+	lista_cobra.append(lista_cabeca)
+
+	if lista_cobra.count(lista_cabeca) > 1:
+		morreu = True
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				exit()
+			if event.type == KEYDOWN:
+				if event.key == K_r:
+						reinicar_jogo()
+						
+	if len(lista_cobra) > comprimento_inicial:
+		del lista_cobra[0]
+
+	aumenta_cobra(lista_cobra)
 
 
-	posicao_cabeca = [cabeça_cobra[0], cabeça_cobra[1]]
-	corpo_cobra.append(posicao_cabeca)
-	
-	if cabeça_cobra.colliderect(comida):
-		tamanho_cobra += 1
-		estruturas.comeu(comida)
-	if tamanho_cobra != 0:
-		for i in range(tamanho_cobra):
-			desenho_cobra.append = pygame.draw.rect(tela, (0, 255, 0), cabeça_cobra[0] - 1, cabeça_cobra[1] - 1, 30, 30)
-			del corpo_cobra
-
-
-		
-	
-	
-		
-	
-	
-
+	tela.blit(texto_formatado, (450, 40))
 	pygame.display.update()
+
